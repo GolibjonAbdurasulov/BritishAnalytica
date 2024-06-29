9ad2d1a9ef52f6da0b1dee8eb1248ae37b439313
Developr <golibjonabdurasulov47@gmail.com>
2024-06-29 16:42:22 +0500

    .

--- BritishAnalytica/Web/Program.cs
+++ BritishAnalytica/Web/Program.cs
@@ -1,19 +1,5 @@
 using DatabaseBroker;
-using DatabaseBroker.Repositories.AboutBusinessModelRepository;
-using DatabaseBroker.Repositories.Common;
-using DatabaseBroker.Repositories.ContactRepository;
-using DatabaseBroker.Repositories.FaqQuestionsRepository;
-using DatabaseBroker.Repositories.FileRepository;
-using DatabaseBroker.Repositories.HomeModelRepository;
-using DatabaseBroker.Repositories.MottoRepository;
-using DatabaseBroker.Repositories.NewsRepository;
-using DatabaseBroker.Repositories.OurServicesRepository;
-using DatabaseBroker.Repositories.PpsRepository;
-using DatabaseBroker.Repositories.ServicePercentRepository;
-using DatabaseBroker.Repositories.TeamMemberRepository;
-using DatabaseBroker.Repositories.UserRepository;
 using Microsoft.EntityFrameworkCore;
-using Services.Interfaces;
 using Services.Services;
 using Web.Extension;
 
@@ -30,20 +16,6 @@ builder.Services.AddDbContextPool<DataContext>(optionsBuilder =>
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
- // builder.Services.AddScoped<IUserRepository, UserRepository>();
- // builder.Services.AddScoped<IAboutBusinessModelRepository, AboutBusinessModelRepository>();
- // builder.Services.AddScoped<IContactRepository, ContactRepository>();
- // builder.Services.AddScoped<IFaqQuestionRepository, FaqQuestionRepository>();
- // builder.Services.AddScoped<IFileRepository, FileRepository>();
- // builder.Services.AddScoped<IHomeModelRepository, HomeModelRepository>();
- // builder.Services.AddScoped<IMottoRepository, MottoRepository>();
- // builder.Services.AddScoped<INewsRepository, NewsRepository>();
- // builder.Services.AddScoped<IOurServicesRepository, OurServicesRepository>();
- // builder.Services.AddScoped<IPpsRepository, PpsRepository>();
- // builder.Services.AddScoped<IServicePercentRepository, ServicePercentRepository>();
- // builder.Services.AddScoped<ITeamMemberRepository, TeamMemberRepository>();
-
 
 builder.Services.ConfigureRepositories();
 
