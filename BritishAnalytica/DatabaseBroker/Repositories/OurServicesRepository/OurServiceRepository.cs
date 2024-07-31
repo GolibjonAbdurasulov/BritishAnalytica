50e1c33a1e15b6002ab081c9c3874b159b13bc25
Developr <golibjonabdurasulov47@gmail.com>
2024-07-31 17:05:04 +0500

    bug fix

--- BritishAnalytica/DatabaseBroker/Repositories/OurServicesRepository/OurServiceRepository.cs
+++ BritishAnalytica/DatabaseBroker/Repositories/OurServicesRepository/OurServiceRepository.cs
@@ -1,9 +1,10 @@
 using DatabaseBroker.Repositories.Common;
+using Entity.Attributes;
 using Entity.Models.OurService;
 using Microsoft.EntityFrameworkCore;
 
 namespace DatabaseBroker.Repositories.OurServicesRepository;
-
+[Injectable]
 public class OurServiceRepository : RepositoryBase<OurService,long>,IOurServicesRepository
 {
     public OurServiceRepository(DataContext dbContext) : base(dbContext)
--- BritishAnalytica/DatabaseBroker/Repositories/ReasonRepositories/ReasonRepository.cs
+++ BritishAnalytica/DatabaseBroker/Repositories/ReasonRepositories/ReasonRepository.cs
@@ -1,9 +1,10 @@
 using DatabaseBroker.Repositories.Common;
+using Entity.Attributes;
 using Entity.Models.FutureModel;
 using Microsoft.EntityFrameworkCore;
 
 namespace DatabaseBroker.Repositories.ReasonRepositories;
-
+[Injectable]
 public class ReasonRepository : RepositoryBase<Reason,long>,IReasonRepository
 {
     public ReasonRepository(DataContext dbContext) : base(dbContext)
